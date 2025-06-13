0bb78f5d7676de8acb13ac91083e0f90ed304eec
Calafate <soporteadm2021@gmail.com>
2025-06-13 15:22:12 -0300

    Otro 10

--- proyecto_01/settings.py
+++ proyecto_01/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k^s%ccuuxtvlx8g5_p1twxeq#c6hlp23$8)))%ikek6*247v60
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.79.138', 'https://192.168.79.138','http://192.168.79.138', 'localhost', '127.0.0.1' ]
 
 
 # Application definition
@@ -125,8 +125,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+# Directorio donde buscará los archivos estáticos durante el desarrollo
 STATICFILES_DIRS = [ BASE_DIR / "static",]
 
+# Directorio de recolección (solo en producción)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
